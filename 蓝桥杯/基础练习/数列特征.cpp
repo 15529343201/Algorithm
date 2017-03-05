2bc1f5db5fccce967860458f93ecee7ccd457b2c
15529343201 <15529342101@139.com>
2017-03-05 21:57:09 +0800

    add cpp

---
+++ 蓝桥杯/基础练习/数列特征.cpp
@@ -0,0 +1,42 @@
+/*问题描述
+
+给出n个数，找出这n个数的最大值，最小值，和。
+输入格式
+
+第一行为整数n，表示数的个数。
+
+第二行有n个数，为给定的n个数，每个数的绝对值都小于10000。
+输出格式
+输出三行，每行一个整数。第一行表示这些数中的最大值，第二行表示这些数中的最小值，第三行表示这些数的和。
+样例输入
+5
+1 3 -2 4 5
+样例输出
+5
+-2
+11
+数据规模与约定
+1 <= n <= 10000。*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	long long n,i,a,max=-10001,min=10001,sum=0;
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		cin>>a;
+		if(a>max)
+		{
+			max=a;
+		}
+		if(a<min)
+		{
+			min=a;
+		}
+		sum+=a;
+	}
+	cout<<max<<endl<<min<<endl<<sum<<endl;
+	return 0;
+}
